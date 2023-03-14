c80a4fcfc2e65d4ceaf93d7abcbf3db2dd73a3cd
Jay Hughes (tier0) <tier0\jay.hughes@ec2amaz-glweiq.tier0.prd.thf.aws>
2023-03-14 11:16:24 +0000

    add method for workforce data

--- data_dl.R
+++ data_dl.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(rvest)
 library(aws.s3)
 
-source('setup.R')
+source('setup.R') # This contains s3 locations: p_bucket
 
 # Web locations
 apps <- 'https://digital.nhs.uk/data-and-information/publications/statistical/appointments-in-general-practice/'
@@ -42,4 +42,23 @@ for (i in to_move){
   
   }
 
-paste0(work, '31-january-2023') %>% return_links(.)
+paste0(work, '31-january-2023') %>%
+  return_links(.) %>%
+  grep('Practice', ., value = TRUE) %>%
+  download.file(., destfile = 'work.zip', method = 'libcurl')
+
+unzip('work.zip')
+unlink('work.zip')
+list.files() %>% grep('General Practice', ., value = TRUE) -> to_move
+
+for (i in to_move){
+  
+  put_object(file = i,
+             object = paste0('GP_data/jan_23/work/', i),
+             bucket = p_bucket,
+             multipart = TRUE)
+  
+  unlink(i)
+  
+}
+
